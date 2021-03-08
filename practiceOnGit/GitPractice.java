b48b124bbf07ccc239e1f9baa9ae5ea5f18fbf2b
DeepakRaval <ravaldeepak0696@gmail.com>
2021-03-08 14:06:39 +0530

    [jeera code] adding two features Addition and Multiplication

--- practiceOnGit/GitPractice.java
+++ practiceOnGit/GitPractice.java
@@ -1,17 +1,31 @@
 package practiceOnGit;
 
+import java.util.Scanner;
+
 public class GitPractice {
-static int c = 0;
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-if(c<3){
-	c++;
-	main(null);
-	
-}else{
-	return;
-}
-		System.out.println("Hello i am using GIT");
-		System.out.println("changes from romote repository");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int num1 = s.nextInt();
+		int num2 = s.nextInt();
+		
+		int resultAddition = addition(num1,num2);
+		System.out.println("The addition = "+resultAddition);
+		
+		int resultMultiplication =multiplication(num1,num2);
+		System.out.println("The addition = "+resultMultiplication);
+		
 	}
+	
+	static int addition(int a, int b){
+		return a + b;
+	}
+	
+	static int multiplication(int a, int b){
+		return a*b;
+	}
+	
 
 }
