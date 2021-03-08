b00d061eed7c1b09aedf5969d26f3b8b278ec6a0
DeepakRaval <ravaldeepak0696@gmail.com>
2021-03-08 10:47:03 +0530

    [jeera ] change in file

--- practiceOnGit/GitPractice.java
+++ practiceOnGit/GitPractice.java
@@ -1,9 +1,15 @@
 package practiceOnGit;
 
 public class GitPractice {
-
+static int c = 0;
 	public static void main(String[] args) {
-
+if(c<3){
+	c++;
+	main(null);
+	
+}else{
+	return;
+}
 		System.out.println("Hello i am using GIT");
 	}
 
